3d117a5cde04a594f5e626e8fb5b5e187617a47e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-09-07 23:19:10 -0400

    0.0.48

--- types.d.ts
+++ types.d.ts
@@ -40,7 +40,7 @@ export interface BEConfig<TPrimaryProp = any>{
     camelizeOptions?: CamelizeOptions<TPrimaryProp>;
     defaultBucket?: string; //TODO
     isParsedProp?: string;
-    noTemplInstantiation?: boolean;
+    attachWhenConnected?: boolean;
 }
 
 export interface CamelizeOptions<TPrimaryProp = any>{

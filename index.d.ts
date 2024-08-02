146f0ffdd2331273cf4912d91c1a00cb4c6f769d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-01 22:20:52 -0400

    0.0.122

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,31 @@
-export * from './ts-refs/be-enhanced/types';
+import { Actions, Compacts, Handlers, Infractions, Positractions, Hitches } from 'trans-render/O.d.ts';
+export {IEnhancement, BEAllProps} from 'trans-render/be.d.ts';
+
+export type Enhancement = string; //camelCase;
+export type EnhKey = string; //lisp case;
+export type FQN = string;
+
+export interface PropInfo{
+    dry?: boolean;
+    ro?: boolean;
+    def?: any;
+    propName?: string,
+}
+
+export type PropLookup = {[key: string]: PropInfo}
+
+export interface BEConfig<TProps = any, TActions = TProps, ETProps = TProps>{
+    propDefaults?: Partial<{[key in keyof TProps]: TProps[key]}>;
+    propInfo?: Partial<{[key in keyof TProps]: PropInfo}>;
+    actions?: Actions<TProps, TActions>;
+    /**
+     * inferred actions
+     */
+    infractions?: Infractions<TProps>,
+    compacts?: Compacts<TProps, TProps & TActions>;
+    hitch?: Hitches<TProps, TActions>
+    handlers?: Handlers<ETProps, TActions>;
+    positractions?: Positractions<TProps, TActions>;
+    watchedBranches?: Set<number>;
+}
+
